809a099b25326a0456a87dd586b6ffcd44dd2598
Paul Reiners <paul.reiners@gmail.com>
2016-03-05 03:56:13 -0600

    fix: Setting default gamma

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -117,10 +117,10 @@ def run(gamma):
     e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.01)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=100)  # press Esc or close pygame window to quit
+    sim = Simulator(e, update_delay=1.0)  # reduce update_delay to speed up simulation
+    sim.run(n_trials=10)  # press Esc or close pygame window to quit
 
 
 if __name__ == '__main__':
-    gamma = float(sys.argv[1])
+    gamma = 0.9
     run(gamma=gamma)
